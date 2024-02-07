1fec5e1cb7fb538892de85b08636eb7ae3ce4014
jimtete <tetepoulidis@ieee.org>
2024-02-07 14:34:15 +0200

    initial implementation

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import time
+from twitch_downloader import download_livestream
+
+twitch_link = "https://www.twitch.tv/ninjacakeassassin"
+
+def parse_timer(v):
+    try:
+        result = int(v)
+        return result
+    except ValueError:
+        print("Error: Unable to parse the string as an integer.")
+        sys.exit(1)
+
+
+def watcher(seconds):
+    print("Watching twitch channel:", twitch_link)
+    while True:
+        # Do something
+        print(download_livestream(twitch_link))
+
+        time.sleep(seconds)
+
+
+try:
+    timer = sys.argv[1]  # First argument: Time in seconds to repeat the ping
+except IndexError:
+    print("Timer cannot be null")
+    sys.exit(1)
+
+timer = sys.argv[1]
+
+timer = parse_timer(timer)
+
+if (timer <= 0):
+    print("Error: Time cannot be less than zero.")
+    sys.exit(2)
+
+watcher(seconds=timer)
+sys.exit(0)
---
+++ twitch_downloader.py
@@ -0,0 +1,18 @@
+import yt_dlp
+import time
+
+def download_livestream(channel_url):
+    return is_channel_live(channel_url, str(int(time.time()))+"_live.mp4")
+
+def is_channel_live(channel_url, file_name):
+    ydl_opts = {
+        'outtmpl': file_name,
+        'fixup': 'never',
+        'verbose': True
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([channel_url])
+    except:
+        print("Ninjacakeassassin is not currently online")
